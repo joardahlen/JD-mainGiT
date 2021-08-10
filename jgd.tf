398612b100110a1336f04da7be0e8ac9bea3d29d
joardahlen <59537384+joardahlen@users.noreply.github.com>
2021-08-10 14:44:31 +0200

    Update jgd.tf

--- jgd.tf
+++ jgd.tf
@@ -1,2 +1,11 @@
 # bare for å sjekke fil
 # dette lages på web'en skal kopieres ned til min PC
+
+# dette ser ut til å fungere utmerket.
+# Kommandoer som er nyttige:
+*/
+  git push
+  git pull
+  git status
+/*
+  
